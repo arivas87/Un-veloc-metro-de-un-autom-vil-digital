c1a6e46f1092b64bb08b98d8e4d1c34382183014
arivas87 <arivas@siketing.com>
2016-03-04 16:03:32 +0100

    Completo a falta de revisar

--- Un velocímetro de un automóvil digital.playground/Contents.swift
+++ Un velocímetro de un automóvil digital.playground/Contents.swift
@@ -2,4 +2,46 @@
 
 import UIKit
 
-var str = "Hello, playground"
+enum Velocidades: Int {
+    case Apagado = 0
+    case VelocidadBaja = 20
+    case VelocidadMedia = 50
+    case VelocidadAlta = 120
+    
+    init(velocidadInicial: Velocidades) {
+        self = velocidadInicial
+    }
+}
+
+class Auto {
+    var velocidad: Velocidades
+    
+    init() {
+        velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad() -> (actual: Int, velocidadEnCadena: String) {
+        switch velocidad {
+        case .Apagado:
+                velocidad = Velocidades.VelocidadBaja
+                return (velocidad.rawValue, "VelocidadBaja")
+        case .VelocidadBaja:
+            velocidad = Velocidades.VelocidadMedia
+            return (velocidad.rawValue, "VelocidadMedia")
+        case .VelocidadMedia:
+            velocidad = Velocidades.VelocidadAlta
+            return (velocidad.rawValue, "VelocidadAlta")
+        case .VelocidadAlta:
+            velocidad = Velocidades.VelocidadMedia
+            return (velocidad.rawValue, "VelocidadMedia")
+        }
+    }
+}
+
+
+let auto = Auto()
+
+for i in 1...20 {
+    let resultado = auto.cambioDeVelocidad()
+    print("\(resultado.actual), " + resultado.velocidadEnCadena)
+}
